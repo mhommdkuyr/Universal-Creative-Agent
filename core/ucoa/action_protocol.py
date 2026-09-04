80bf16f490c7bdb900a994324ec60cfe6e1e1236
mhommdkuyr <776117073moo@gmail.com>
2026-09-04 21:42:03 +0300

    feat(core): extend Android action protocol for universal computer use

--- core/ucoa/action_protocol.py
+++ core/ucoa/action_protocol.py
@@ -1,8 +1,13 @@
 import json
 from .models import Action
 
+
 def encode_android_action(action: Action) -> str:
-    allowed={'tap','swipe','long_press','type_text','click_text','back','home','open_app','screenshot'}
+    allowed = {
+        'tap', 'swipe', 'long_press', 'type_text', 'click_text', 'back', 'home',
+        'open_app', 'screenshot', 'open_url', 'open_app_by_name', 'click_any_text',
+        'type_into_any', 'observe', 'wait'
+    }
     if action.type not in allowed:
         raise ValueError(f'unsupported Android action: {action.type}')
-    return json.dumps({'action':action.type, **action.args}, ensure_ascii=False)
+    return json.dumps({'action': action.type, **action.args}, ensure_ascii=False)
